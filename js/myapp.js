033699b431e0dbbaa8c279b7857dbc638f6c500c
JesusAguiarSK <j.aguiar@softwarekeep.com>
2022-09-15 20:53:23 -0400

    js

---
+++ js/myapp.js
@@ -0,0 +1,72 @@
+// function setdetails() { 
+
+// }
+
+//I need to add a listener on click, so when the set button is clicked
+//the div's attributes are modified.
+
+
+
+
+//We gotta select all the css properties as an array
+
+let property = document.querySelectorAll('select[name="cssProperty"]');
+
+console.log(property);
+
+// Now we gotta make an array of the options the user 
+// has selected 
+
+function getSelectedDetails {
+
+	// var e = document.getElementById("background");
+	// var value = e.options[e.selectedIndex].value;
+	// var text = e.options[e.selectedIndex].text;
+
+	// var e = document.getElementById("width");
+	// var value = e.options[e.selectedIndex].value;
+	// var text = e.options[e.selectedIndex].text;
+
+	// var e = document.getElementById("height");
+	// var value = e.options[e.selectedIndex].value;
+	// var text = e.options[e.selectedIndex].text;
+
+	console.log(text);
+
+};
+
+
+//We gotta select the div
+
+var changebox = document.getElementById('modify');
+
+function { 
+
+	changebox.addEventListener('click', function {
+		//modify the modify variable for the concat of the selected values
+	}) 
+
+};
+
+
+
+// let i;
+
+// for( let i = 0; i < counter.length; i++) 
+// {
+// 	console.log(counter[i]);
+// }
+
+
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener('click', function() {
+//     this.classList.toggle('active');
+//     let content = this.nextElementSibling;
+//     if (content.style.display === 'flex') {
+//       content.style.display = 'none';
+//     } else {
+//       content.style.display = 'flex';
+//     }
+//   });
+// }
