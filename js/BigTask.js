b46969be7459b4562cdbbfb4495ad6651fcb4758
JesusAguiarSK <j.aguiar@softwarekeep.com>
2022-09-21 11:01:30 -0400

    BigTask update
    BigTask update

--- js/BigTask.js
+++ js/BigTask.js
@@ -1,4 +1,4 @@
-//I need a different geolocation API
+//I need a different geolocation API/
 
 // const cors = require('cors');
 
@@ -6,3 +6,50 @@
 
 //   .then( res => res.json())
 //   .then(data => console.log(data));
+
+// var geoArray = //data from the fetch//  
+
+
+//I need a different geolocation API/
+
+// let geoArrayDefault = geoArray.method to extract the below values.
+      //Information
+        // Street,
+        // City,
+        // State
+        // Country
+      //link to map
+        //map
+
+//Paste the link to the map to the iframe, or do I need to use another API to extract the iframe?
+
+//Convert the information it to a list in html?
+
+document.getElementById('button-group-box').addEventListener('click', function(event) {
+
+      
+      // var geoArrayFilter = geoArray.method to extract the above values.
+
+      //Information
+        // Street,
+        // City,
+        // State
+        // Country
+      //link to map
+        //map
+
+
+ 
+
+    //check if post code is valid
+
+    if valid
+
+
+
+
+
+    
+
+});
+
