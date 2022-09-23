e531ed341298929b39654d774a298fcaba60dca7
JesusAguiarSK <j.aguiar@softwarekeep.com>
2022-09-22 21:12:38 -0400

    Last Commit of the day

--- js/BigTask.js
+++ js/BigTask.js
@@ -8,7 +8,7 @@ fetch( 'https://app.zipcodebase.com/api/v1/search?apikey=24d96260-3a7b-11ed-a18a
 
 console.log(geoArray)
 
-
+// set a default value for all the target classes in the html. 
 
 // let geoArrayDefault = geoArray.method to extract the below values.
       //Information
@@ -19,13 +19,14 @@ console.log(geoArray)
       //link to map
         //map
 
-//Paste the link to the map to the iframe, or do I need to use another API to extract the iframe?
+//Paste the link to the map to the iframe, or do I need to use another API to extract the iframe (like
+//putting the latitude/longitude in google maps and pull out images?
 
-//Convert the information it to a list in html?
+//Target each span class with the array's information.
 
 // document.getElementById('button-group-box').addEventListener('click', function(event) {
 
-// //check if post code is valid
+// Check if post code is valid
 
 //     if valid
 
@@ -40,6 +41,6 @@ console.log(geoArray)
 //         //map
 
 //     else
-//       document.getElementById('zip-code').addEventListener('', function (event) {
+//       document.getElementById('zip-code').addEventListener('click', function (event) {
 //         //reset content of zip code
 //       });
